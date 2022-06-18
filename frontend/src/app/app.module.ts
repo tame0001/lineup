a318a22195f83390aaa8c2b05e7688597969a6f6
tame0001 <thirawat.tam@gmail.com>
2022-06-18 18:46:26 -0400

    update to angular 13

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +14,7 @@ import { VolleyballComponent } from './volleyball/volleyball.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    NgbModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
