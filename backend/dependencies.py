baa4a2fa396304fc5e572a168dd26ca18978663f
tame0001 <thirawat.tam@gmail.com>
2025-09-27 21:52:30 -0400

    add user form

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,4 +1,9 @@
+from typing import Annotated
 from sqlmodel import create_engine, Session
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+
+from .models import User
 
 # Connect to the SQLite database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sql"  # Adjust the path as needed
@@ -7,7 +12,18 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 def get_db():
     with Session(engine) as session:
         yield session
+
+
+def fake_decode_token(token):
+    return User(id=1, name=token, email="test@example.com")
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    # Placeholder for actual user retrieval logic
+    return fake_decode_token(token)
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-
+from .dependencies import get_current_user
+from .models import User
 from .routers import user, week, lineup, rsvp
+
+fake_users_db = {
+    "john": {
+        "username": "john",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "password",
+        "disabled": False,
+    }
+}
 
 app = FastAPI()
 app.include_router(user.router)
@@ -24,6 +35,21 @@ app.add_middleware(
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    """
+    Endpoint to handle user login and token generation.
+    """
+    user_dict = fake_users_db.get(form_data.username)
+    print(user_dict)
+    if not user_dict or form_data.password != user_dict["hashed_password"]:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    return {"access_token": user_dict["username"], "token_type": "bearer"}
+
+
+@app.get("/me")
+async def read_current_user(current_user: Annotated[User, Depends(get_current_user)]):
+    """
+    Retrieve the currently authenticated user.
+    """
+    return current_user
