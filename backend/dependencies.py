aced0d883c3aa23c101bdce0cc5dd67d7318bfdc
tame0001 <thirawat.tam@gmail.com>
2025-10-18 17:18:18 -0400

    dependency template

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -14,6 +14,16 @@ engine = create_engine(
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+fake_users_db = {
+    "john": {
+        "username": "john",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "password",
+        "disabled": False,
+    }
+}
+
 
 def get_db():
     with Session(engine) as session:
@@ -27,3 +37,6 @@ def fake_decode_token(token):
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     # Placeholder for actual user retrieval logic
     return fake_decode_token(token)
+
+
+# async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
