d851ba6ca174aadc55f39b39f72ded1bfea243d2
tame0001 <thirawat.tam@gmail.com>
2026-02-21 14:40:26 -0500

    remove filter

--- frontend/src/app/admin/admin.ts
+++ frontend/src/app/admin/admin.ts
@@ -1,8 +1,5 @@
 import { Component, effect, inject, signal, OnInit } from '@angular/core';
 import { map } from 'rxjs';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 
 import { PlayerCard } from './player-card/player-card';
 import { BackendService } from '../backend-service';
@@ -11,26 +8,33 @@ import { SelectWeek } from '../select-week/select-week';
 
 @Component({
   selector: 'app-admin',
-  imports: [
-    PlayerCard,
-    SelectWeek,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [PlayerCard, SelectWeek],
   templateUrl: './admin.html',
   styleUrl: './admin.scss',
 })
 export class Admin implements OnInit {
   private _backend = inject(BackendService);
   players = signal<Player[]>([]); // All players fetched from the backend
-  filteredPlayers = signal<Player[]>([]);
-  rsvps = signal<number[]>([]);
-  weekID = signal<number>(1);
-  filteredPlayerName = signal<string>('');
+  rsvps = signal<number[]>([]); // List of user IDs who have RSVP'd 'in'
+  weekID = signal<number>(0); // Currently selected week ID
   n_player_in = signal<number>(0); // Number of players who have RSVP'd 'in'
 
   constructor() {
+    effect(() => {
+      // Preventing fetching RSVPs for weekID 0, which is the initial state
+      if (this.weekID() > 0) {
+        // Fetch RSVPs for the selected week and update the rsvps signal
+        this._backend.getWeekRSVPs(this.weekID()).subscribe((rsvps) => {
+          // Extract user IDs from the RSVPs
+          this.rsvps.set(rsvps.map((rsvp) => rsvp.user_id));
+          // Count how many players have RSVP'd
+          this.n_player_in.set(this.rsvps().length);
+        });
+      }
+    });
+  }
+
+  ngOnInit() {
     this._backend
       // Fetch players from the backend
       .getPlayers()
@@ -41,35 +45,13 @@ export class Admin implements OnInit {
       .subscribe((players) => {
         // Sort players alphabetically by name
         players = players.sort((a, b) => a.name.localeCompare(b.name));
+        // update player list
         this.players.set(players);
       });
-
-    effect(() => {
-      this._backend.getWeekRSVPs(this.weekID()).subscribe((rsvps) => {
-        this.rsvps.set(rsvps.map((rsvp) => rsvp.user_id));
-        this.n_player_in.set(this.rsvps().length);
-      });
-    });
-
-    effect(() => {
-      const filter = this.filteredPlayerName().toLowerCase();
-      if (!filter) {
-        this.filteredPlayers.set([...this.players()]);
-      } else {
-        this.filteredPlayers.set(
-          this.players().filter((player) =>
-            player.name.toLowerCase().includes(filter),
-          ),
-        );
-      }
-    });
-  }
-
-  ngOnInit() {
-    this.filteredPlayers.set([...this.players()]);
   }
 
   updateWeekID(weekID: number) {
+    // Update the weekID signal when a new week is selected
     this.weekID.set(weekID);
   }
 }
