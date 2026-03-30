d744a60583b1c6f007f6519b4ca8ecf4d7ad830a
tame0001 <thirawat.tam@gmail.com>
2026-03-29 22:49:13 -0400

    add player card in the roster page

--- frontend/src/app/admin/roster/roster.ts
+++ frontend/src/app/admin/roster/roster.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+import { BackendService } from '../../backend-service';
+import { Player } from '../../data-interface';
 
 @Component({
   selector: 'app-roster',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './roster.html',
   styleUrl: './roster.scss',
 })
-export class Roster {}
+export class Roster implements OnInit {
+  private _backend = inject(BackendService);
+  roster = signal<Player[]>([]); // All players fetched from the backend
+
+  ngOnInit() {
+    // Fetch the roster data from the backend when the component initializes
+    this._backend.getPlayers().subscribe((roster) => {
+      // Sort players alphabetically by name
+      roster = roster.sort((a, b) => a.name.localeCompare(b.name));
+      // update roster list
+      this.roster.set(roster);
+    });
+  }
+}
